41ed52752556d89055c717040e2fa4f93732f16e
Smart <chukwusmartchukwuemeka@gmail.com>
2025-11-12 13:43:13 +0100

    feat: initial commit cont'd

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,12 +1,4 @@
-import Button from "@/components/ui/button";
-import Toast from "@/components/ui/toast";
-import React from "react";
 
 export default function Auth() {
-  return (
-    <div>
-      <Button />
-      <Toast />
-    </div>
-  );
+  return <div></div>;
 }
--- src/components/ui/toast.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Toast() {
-  return (
-    <div>toast</div>
-  )
-}
